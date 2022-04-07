728d7ced90d604663263f1102ecdc4eae9302a9c
jinsu <jinsu350@gmail.com>
2022-04-07 23:45:54 +0900

    UPDATE: Define abstract functions in NewsRepository.kt
    define abstract functions based on use cases about news

--- app/src/main/java/com/jsbdev/newsapp/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/jsbdev/newsapp/domain/repository/NewsRepository.kt
@@ -1,4 +1,15 @@
 package com.jsbdev.newsapp.domain.repository
 
+import com.jsbdev.newsapp.data.model.APIResponse
+import com.jsbdev.newsapp.data.model.Article
+import com.jsbdev.newsapp.data.util.Resource
+import kotlinx.coroutines.flow.Flow
+
 interface NewsRepository {
+
+    suspend fun getNewsHeadlines(): Resource<APIResponse>
+    suspend fun getSearchedNews(searchQuery: String): Resource<APIResponse>
+    suspend fun saveNews(article: Article)
+    suspend fun deleteNews(article: Article)
+    fun getSavedNews(): Flow<List<Article>>
 }
